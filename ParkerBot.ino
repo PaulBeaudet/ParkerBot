84286c53727511df75b1fc10bc1e1e6842a932eb
paulBeaudet <inof8or@gmail.com>
2016-05-07 15:31:52 -0400

    clean up some whitespace and suggest a couple of things

--- ParkerBot.ino
+++ ParkerBot.ino
@@ -1,248 +1,235 @@
-#include <IRLib.h>
+// built w/1.6.5 version of Arduino IDE
+#include <IRLib.h> // https://learn.adafruit.com/using-an-infrared-library/overview
 #include <IRLibMatch.h>
 #include <IRLibRData.h>
 #include <IRLibTimer.h>
 
-const int IRPin = 2; 
-const long FORWARD = 0xFF18E7 ; 
+const int IR_PIN = 2;
+const long FORWARD = 0xFF18E7;
 const long STOP = 0xFF38C7;
-const long BACKWARD = 0xFF4AB5 ; 
-const long RIGHT = 0xFF5AA5 ; 
-const long LEFT = 0xFF10EF ; 
+const long BACKWARD = 0xFF4AB5;
+const long RIGHT = 0xFF5AA5;
+const long LEFT = 0xFF10EF;
 const long REMOTETOGGLE = 0xFFE21D;
 
-const int AIA = 9;  // (pwm) pin 9 connected to pin A-IA
-const int AIB = 5;  // (pwm) pin 5 connected to pin A-IB
-const int BIA = 10; // (pwm) pin 10 connected to pin B-IA 
-const int BIB = 6;  // (pwm) pin 6 connected to pin B-IB
+const int AIA = 9;  //(pwm) pin 9 connected to pin A-IA
+const int AIB = 5;  //(pwm) pin 5 connected to pin A-IB
+const int BIA = 10; //(pwm) pin 10 connected to pin B-IA
+const int BIB = 6;  //(pwm) pin 6 connected to pin B-IB
 
-const int LeftBumperPin = 4;
-const int RightBumperPin = 3 ; 
-const int pingPin = 11;
-const int echoPin = 13;
-const int ledPinEdgeDetector1 = 7 ;
-const int ledPinEdgeDetector2 = 8 ; 
+const int LEFT_BUMPER = 4;
+const int RIGHT_BUMPER = 3;
+const int PING = 11;
+const int ECHO = 13;
+const int LED_EDGE_DETECT_1 = 7;
+const int LED_EDGE_DETECT_2 = 8;
 
+// Global vars
 byte speed = 200;  // change this (0-255) to control the speed of the motors
 byte halfspeed = 200 ;
 
-//Create a receiver object to listen on pin 11
-IRrecv My_Receiver(IRPin);
+long pingTimer = 0;
+long ldrTimer = 0;
+long cm = 25;
+long ldrNoLedEmissions = 0;
+int ldrInterval = 150;
+boolean usingRemote = false;
 
-//Create a decoder object
-IRdecode My_Decoder;
- 
+IRrecv My_Receiver(IR_PIN); // Create a receiver object to listen
+IRdecode My_Decoder;        // Create a decoder object
 
 void setup() {
-  
   // h bridge pins
   pinMode(AIA, OUTPUT); // set pins to output
   pinMode(AIB, OUTPUT);
   pinMode(BIA, OUTPUT);
   pinMode(BIB, OUTPUT);
-
-// bumpers 
-  pinMode(LeftBumperPin, INPUT_PULLUP) ; 
-  pinMode(RightBumperPin, INPUT_PULLUP) ; 
-
-// ultrasound pins
-  pinMode(pingPin, OUTPUT);
-  pinMode(echoPin, INPUT) ; 
-
-  // edge detector setup 
-  pinMode(ledPinEdgeDetector1, OUTPUT) ; 
-  pinMode(ledPinEdgeDetector2, OUTPUT) ; 
- 
-  //forward();
- 
-   Serial.begin(115200) ; 
+  // switch bumpers
+  pinMode(LEFT_BUMPER, INPUT_PULLUP);
+  pinMode(RIGHT_BUMPER, INPUT_PULLUP);
+  // ultrasonic ping sensor pins
+  pinMode(PING, OUTPUT);
+  pinMode(ECHO, INPUT);
+  // edge detector setup
+  pinMode(LED_EDGE_DETECT_1, OUTPUT);
+  pinMode(LED_EDGE_DETECT_2, OUTPUT);
+  Serial.begin(115200);
   My_Receiver.enableIRIn(); // Start the receiver
- 
 }
 
-long pingTimer = 0 ; 
-long ldrTimer = 0 ; 
-long cm = 25 ;
-long ldrNoLedEmissions = 0 ; 
-int ldrInterval = 150 ; 
-
-boolean usingRemote = false ;
-
-
-
 void loop() {
-
+  // remoteReact(); // react to remote without using a switch case <---!!! uncomment - comment out 68 to 110 - to test
   if (My_Receiver.GetResults(&My_Decoder)) {
     My_Decoder.decode();
     My_Decoder.DumpResults();
      
-    if (My_Decoder.decode_type== NEC) {
-           
+    if (My_Decoder.decode_type== NEC){
       switch(My_Decoder.value) {
         case FORWARD:  //Volume Down
-          if(usingRemote == true) 
-          {
-          forward() ;
-          Serial.println("Forward call through the remote") ; 
+          if(usingRemote == true){
+            forward();
+            Serial.println("Forward call through the remote");
           }
           break;
-        case STOP:  //Play/Pause
-           if(usingRemote == true) 
-          {
-          stopMotor() ;
-          } 
+        case STOP:                               //Play/Pause
+          if(usingRemote == true){stopMotor();}
           break;
-        case RIGHT:  //Volume Up
-          if(usingRemote == true) 
-          {
-            right() ; 
-            delay(250) ; 
-            stopMotor() ; 
-          } 
+        case RIGHT:                              //Volume Up
+          if(usingRemote == true){
+            right();
+            delay(250);
+            stopMotor();
+          }
           break;
-        case LEFT:  
-          if(usingRemote == true) 
-          {
-            left() ;
-            delay(250) ; 
-            stopMotor() ; 
-          } 
+        case LEFT:
+          if(usingRemote == true){
+            left();
+            delay(250);
+            stopMotor();
+          }
           break;
         case BACKWARD: 
-           if(usingRemote == true) 
-          {
-            backward() ; 
- 
-          } 
+          if(usingRemote == true){backward();}
           break;
         case REMOTETOGGLE:
-          if(usingRemote == false) 
-          {
-            usingRemote = true ;
-            stopMotor() ; 
-          } 
-          else 
-            usingRemote = false ;
-          Serial.println(usingRemote) ; 
-          break ;  
+          if(usingRemote == false){
+            usingRemote = true;
+            stopMotor();
+          } else {usingRemote = false;}
+          Serial.println(usingRemote);
+          break;
       }
-      
     }
     My_Receiver.resume(); //Restart the receiver
   }
   
-  if (millis() > ldrTimer && usingRemote == false )
-  {
+  if (millis() > ldrTimer && usingRemote == false ){
     // 10 ms or 150 ms timer?
-     if ( ldrInterval == 150 ) 
-     {
-       ldrInterval = 10 ;
+     if ( ldrInterval == 150 ){
+       ldrInterval = 10;
        ldrTimer = millis() + ldrInterval ; 
-       digitalWrite(ledPinEdgeDetector2, HIGH) ; 
-       digitalWrite(ledPinEdgeDetector1, HIGH) ; 
-     }
-     else 
-     {
-       ldrInterval = 150 ; 
-       ldrTimer = millis() + ldrInterval ; 
+       digitalWrite(LED_EDGE_DETECT_2, HIGH);
+       digitalWrite(LED_EDGE_DETECT_1, HIGH);
+     } else {
+       ldrInterval = 150;
+       ldrTimer = millis() + ldrInterval;
        long ldrLedWithEmissions = analogRead(A0);
-       
-       if (ldrLedWithEmissions < 510 )
-       { 
-         digitalWrite(ledPinEdgeDetector2, LOW) ; 
-         digitalWrite(ledPinEdgeDetector1, LOW) ;
-  
-         stopAndBackup() ; 
-       }
-       else
-       {
-         digitalWrite(ledPinEdgeDetector2, LOW) ; 
-         digitalWrite(ledPinEdgeDetector1, LOW) ;
+       if (ldrLedWithEmissions < 510 ){
+         digitalWrite(LED_EDGE_DETECT_2, LOW);
+         digitalWrite(LED_EDGE_DETECT_1, LOW);
+         stopAndBackup();
+       } else {
+         digitalWrite(LED_EDGE_DETECT_2, LOW);
+         digitalWrite(LED_EDGE_DETECT_1, LOW);
        }
      }
   }
 
-  if ( millis() > pingTimer  && usingRemote == false) 
-   {
-      pingTimer = millis() + 250 ; 
-      
-      digitalWrite(pingPin, LOW);
+  if ( millis() > pingTimer  && usingRemote == false) {
+      pingTimer = millis() + 250;
+      digitalWrite(PING, LOW);
       delayMicroseconds(2);
-      digitalWrite(pingPin, HIGH);
+      digitalWrite(PING, HIGH);
       delayMicroseconds(2);
-      digitalWrite(pingPin, LOW);
+      digitalWrite(PING, LOW);
 
-      long duration = pulseIn(echoPin, HIGH);
+      long duration = pulseIn(ECHO, HIGH);
       cm = microsecondsToCentimeters(duration);
-
-//      Serial.print("distance in cm = " ) ; 
-//      Serial.println(cm) ; 
-//      Serial.print("left bumper " ) ; 
-//      Serial.println(digitalRead(LeftBumperPin)) ; 
-//      Serial.print("right bumper " ) ; 
-//      Serial.println(digitalRead(RightBumperPin)) ; 
   }
  
-  if (digitalRead(LeftBumperPin) == HIGH || digitalRead(RightBumperPin) == HIGH || cm <= 10  && usingRemote == false)
-  {
-    stopAndBackup() ; 
+  if (digitalRead(LEFT_BUMPER) == HIGH || digitalRead(RIGHT_BUMPER) == HIGH || cm <= 10  && usingRemote == false){
+    stopAndBackup();
   }
- }
-void stopAndBackup() 
-{
-  stopMotor(1000);
-    
-    backward(750) ; 
-     
-
-    stopMotor(1000) ; 
-    
-
-    if ((millis() % 2) == 0 ) 
-      left(250) ;
-    else 
-      right(250) ; 
-
-    stopMotor(1000) ; 
-    delay(1000) ; 
-    
-    forward() ;
-  
 }
 
-void stopMotor()
-{
+// -- end of main loop ---
+
+void remoteReact(){                            // react to IR remote commands (should do same thing as switch logic... in theory)
+  if (My_Receiver.GetResults(&My_Decoder)){    // if IR reciever returns results
+    My_Decoder.decode();                       // decode those results
+    My_Decoder.DumpResults();                  // dump result?
+    if (My_Decoder.decode_type == NEC) {       // given we are getting a result from the right remote? can we take out this condition?
+      if(usingRemote){                         // when using the remote
+        if(My_Decoder.value == FORWARD){       // 1? button
+          forward();
+          Serial.println("Forward call through the remote");
+        } else if (My_Decoder.value == STOP){  // 5? button
+          stopMotor();
+        } else if (My_Decoder.value == RIGHT){ // 6? button
+          right();
+          delay(250);
+          stopMotor();
+        } else if (My_Decoder.value == LEFT){  // 4? button
+          left();
+          delay(250);
+          stopMotor();
+        } else if (My_Decoder.value == BACKWARD){ // 8? button
+          backward();
+        }
+      }
+      if(My_Decoder.value == REMOTETOGGLE){ // whether using remote or not check if we want to
+        usingRemote = !usingRemote;         // toggle using remote from previous state
+        if(usingRemote){stopMotor();}       // make sure motors are stopped when taking control
+        Serial.println(usingRemote);
+      }
+    }
+  }
+  My_Receiver.resume(); //Restart the receiver
+}
+
+void stopAndBackup(){
+  stopMotor();
+  backward();
+  stopMotor();
+  if((millis() % 2) == 0 ){
+    left();
+  } else {
+    right();
+  }
+  stopMotor();
+  delay(1000);
+  forward();
+}
+
+/*
+ * two of pins being writen actually only need digitalWrite,
+ * if using non-pwm pin this routine will fail, because it expects to be able to analog write
+ * which is completly ok, h-bridge can recieve an analog value for its digital direction pins,
+ * but as far as its concerned it just got a high or a low no mater the voltage
+ * in fact if the pwm was slow enough and h-bridge sampled fast enough the motors would never go anywhere either 
+ * They would never be able to ramp up fast enough to go either direction
+ * AKA the pulse would constantly change the direction too fast to move
+ */
+
+void stopMotor(){
   analogWrite(AIA, 0);
   analogWrite(AIB, 0);
   analogWrite(BIA, 0);
   analogWrite(BIB, 0);
 }
  
-void backward()
-{
+void backward(){
   analogWrite(AIA, 0);
   analogWrite(AIB, speed);
   analogWrite(BIA, 0);
   analogWrite(BIB, speed);
 }
 
-void forward()
-{
+void forward(){
   analogWrite(AIA, speed);
   analogWrite(AIB, 0);
   analogWrite(BIA, speed + 20);
   analogWrite(BIB, 0);
 }
-void right()
-{
+
+void right(){
   analogWrite(AIA, halfspeed);
   analogWrite(AIB, 0);
   analogWrite(BIA, 0);
   analogWrite(BIB, halfspeed);
-
 }
-void left()
-{
+
+void left(){
   analogWrite(AIA, 0);
   analogWrite(AIB, halfspeed);
   analogWrite(BIA, halfspeed);
@@ -255,3 +242,4 @@ long microsecondsToCentimeters(long microseconds) {
   // object we take half of the distance travelled.
   return microseconds / 29 / 2;
 }
+
